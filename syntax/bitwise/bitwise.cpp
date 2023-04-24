47e1980a0277c1587abb9a3e25b5cae21925b7ea
5109U25854 <Qingqiu.Li@Sony.com>
2023-04-24 15:58:03 +0800

    初始化C++工程的VSCode环境

---
+++ syntax/bitwise/bitwise.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a=25,b=18,m,n,i,j,k;
+  //按位与 --> 1&1=1 1&0=0 0&1=0 0&0=0
+  m=a&b;  cout << "m=" << m << endl;
+  //按位或 --> 1|1=1 1|0=1 0|1=1 0|0=0
+  n=a|b;  cout << "n=" << n << endl;
+  //按位异或 --> 1^1=0 1^0=1 0^1=1 0^0=0
+  i=a^b;  cout << "i=" << i << endl;
+  //乘以2
+  j=a<<1; cout << "j=" << j << endl;
+  //除以2，注意因为这里都是整型，会去掉小数位，所以得到12
+  k=a>>1; cout << "k=" << k << endl;
+  return 0;
+}
