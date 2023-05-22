8344a9c3860f075e77266795bf6c78d0516f5da9
github_qing <2671800947@qq.com>
2023-05-22 17:58:06 +0800

    complete 1.2.5-gift1

--- usaco/1.2.5-gift/gift1.cpp
+++ usaco/1.2.5-gift/gift1.cpp
@@ -16,42 +16,41 @@ int main()
   int i;
   int temp_int, temp_int2;
   string temp_str, temp_str2;
-  map<string, int> players;
+  map<string, int> players_account;
 
   ifstream fin("gift1.in");
   ofstream fout("gift1.out", ios_base::out);
 
   // init players
   fin >> temp_int;
+  string players_name[temp_int];
   for (i = 0; i < temp_int; i++)
   {
     fin >> temp_str;
-    players[temp_str] = 0;
+    players_account[temp_str] = 0;
+    players_name[i] = temp_str;
   }
 
   // give money circle
-  while (!fin.eof())
+  while (fin >> temp_str)
   {
-    // giver
-    fin >> temp_str;
     // money to give and number of people to share
     fin >> temp_int >> temp_int2;
-    if (temp_int2 != 0)
+    // receiver
+    for (i = 0; i < temp_int2; i++)
     {
-      // receiver
-      for (i = 0; i < temp_int2; i++)
-      {
-        fin >> temp_str2;
-        players[temp_str2] += temp_int / temp_int2;
-      }
-      players[temp_str] += -temp_int + temp_int % temp_int2;
+      fin >> temp_str2;
+      players_account[temp_str2] += temp_int / temp_int2;
     }
+    players_account[temp_str] -= temp_int;
+    if (0 != temp_int2)
+      players_account[temp_str] += temp_int % temp_int2;
   }
 
   // output
-  for (auto it = players.begin(); it != players.end(); ++it)
+  for (i = 0; i < sizeof(players_name) / sizeof(players_name[0]); i++)
   {
-    fout << it-> first << ' ' << it-> second << endl;
+    fout << players_name[i] << ' ' << players_account[players_name[i]] << endl;
   }
 
   return 0;
