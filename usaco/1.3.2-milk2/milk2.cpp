5b3ab0a9b0c7934951a72de92940795a0a89ca93
github_qing <2671800947@qq.com>
2023-05-31 16:22:01 +0800

    complete 1.3.2-milk2

--- usaco/1.3.2-milk2/milk2.cpp
+++ usaco/1.3.2-milk2/milk2.cpp
@@ -1,3 +1,9 @@
+/*
+ID: qq267181
+TASK: milk2
+LANG: C++
+*/
+
 #include <fstream>
 #include <iostream>
 #include <cstring>
@@ -15,8 +21,8 @@ int main()
   int i = 0, j = 0;
   int temp;
   int num_farmer, now_farmer = 0;
-  int interval_1, interval_1_start, interval_1_end;
-  int interval_0, interval_0_start, interval_0_end;
+  int interval_1 = 0, interval_1_head;
+  int interval_0 = 0, interval_0_head;
 
   // input
   ifstream fin("milk2.in");
@@ -25,52 +31,78 @@ int main()
   for (i = 0; i < num_farmer; i++)
   {
     fin >> temp;
-    time.push_back(make_pair(temp, 0));
-    fin >> temp;
+    // 1 stand for enter
     time.push_back(make_pair(temp, 1));
+    // -1 stand for leave
+    fin >> temp;
+    time.push_back(make_pair(temp, -1));
   }
 
   // compute
+
+  // 1.arrange in ascending order
   sort(time.begin(), time.end(), comparePairs);
 
+  vector<pair<int, int>> timef;
   for (std::vector<pair<int, int>>::iterator it = time.begin(); it != time.end(); ++it)
   {
+    if (it != time.end() && (*it).first == (*(it + 1)).first && 0 == (*it).second + (*(it + 1)).second)
+    {
+      ++it;
+    }
+    else
+    {
+      timef.push_back(*it);
+    }
+  }
+
+  // for (std::vector<pair<int, int>>::iterator it = timef.begin(); it != timef.end(); ++it)
+  // {
+  //   cout << (*it).first << ' ' << (*it).second << endl;
+  // }
+
+  for (std::vector<pair<int, int>>::iterator it = timef.begin(); it != timef.end(); ++it)
+  {
+    // 2.compute num of farmer
     // new farmer entered
-    if (0 == (*it).second)
+    if (1 == (*it).second)
     {
-      now_farmer++;
-    }
-    // a farmer left
-    else if (1 == (*it).second)
-    {
-      now_farmer--;
-    }
-    if (0 == now_farmer)
-    {
-      interval_0_start = (*it).first;
-    }
-    else if (0 < now_farmer)
-    {
-      if (0 == (*it).second)
+      // before a >1 farmer gap
+      if (0 == now_farmer)
       {
-        interval_1_start = (*it).first;
-        if (1 == now_farmer)
+        // 3.setup the head of >1 farmer gap
+        interval_1_head = (*it).first;
+      }
+      now_farmer++;
+      // after a 0 farmer gap
+      if (it != timef.begin() && 1 == now_farmer)
+      {
+        // 4.reset the max 0 farmer gap
+        if (interval_0 < (*it).first - interval_0_head)
         {
-          interval_0_end = (*it).first;
+          interval_0 = (*it).first - interval_0_head;
         }
       }
-      else
+    }
+    // a farmer left
+    else if (-1 == (*it).second)
+    {
+      // before a 0 farmer gap
+      if (1 == now_farmer)
       {
-        interval_1_end = (*it).first;
+        // 3.setup the head of 0 farmer gap
+        interval_0_head = (*it).first;
       }
-    }
-    if (interval_1 < interval_1_end - interval_1_start)
-    {
-      interval_1 = interval_1_end - interval_1_start;
-    }
-    if (interval_0 < interval_0_end - interval_0_start)
-    {
-      interval_0 = interval_0_end - interval_0_start;
+      now_farmer--;
+      // after a >1 farmer gap
+      if (0 == now_farmer)
+      {
+        // 4.reset the max >1 farmer gap
+        if (interval_1 < (*it).first - interval_1_head)
+        {
+          interval_1 = (*it).first - interval_1_head;
+        }
+      }
     }
   }
 
